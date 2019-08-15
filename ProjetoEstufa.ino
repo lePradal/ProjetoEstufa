0ace302043368a07973198f6bf9d2c03b054fa78
lePradal <le.pradal.lopes@gmail.com>
2019-08-15 00:44:05 -0300

    Adição da função de Leitura de Umidade de Solo e update da função Média Móvel

--- ProjetoEstufa.ino
+++ ProjetoEstufa.ino
@@ -19,6 +19,7 @@
                             Mapeamento de Hardware                       */
 
 #define dht_pin 5      //Pino do Sinal do DHT11 ligado no digital 5
+#define humSolo analogRead(A0) //Sinal do Sensor LM393 ligado no A0
 
 /*=========================================================================
                             Declaração de Objetos                        */
@@ -33,9 +34,12 @@ dht DHT;              //Objeto para o Sensor
 /*=========================================================================
                               Variáveis Globais                          */
 
-int temperatura = 0, //Armazena a Temperatura em Inteiro
-    umidadeAr = 0;   //Armazena a Umidade do Ar em Inteiro
-int numbers[n];      //Vetor com os valores para a Média Móvel
+int temperatura = 0,    //Armazena a Temperatura em Inteiro
+    umidadeAr = 0,      //Armazena a Umidade do Ar em Inteiro
+    umidadeSolo = 0;    //Armazena a Umidade do Solo em Inteiro
+int temperaturas[n],    //Vetor com os valores para a Média Móvel
+    umidadesAr[n],      //Vetor com os valores para a Média Móvel
+    umidadesSolo[n];    //Vetor com os valores para a Média Móvel
 
 
 /*=========================================================================
@@ -48,9 +52,10 @@ void setup() {
                                  Loop Infinito                           */
 void loop() {
   lerUmidTemp();
-  temperatura = movingAverage(temperatura);
-  umidadeAr = movingAverage(umidadeAr);
-  delay(50);
+  lerUmidSolo();
+  movingAverage();
+  displaySerial();
+  delay(15);
 }
 /*=========================================================================
                                Métodos Adicionais                        */
@@ -77,10 +82,40 @@ void lerUmidTemp(){
   umidadeAr = DHT.humidity;
 }
 
-long movingAverage(int variavelLida){
-  for(int i = n-1; i > 0; i--) numbers[i] = numbers[i - 1];
-  numbers[0] = variavelLida;
-  long acc = 0;
-  for(int i = 0; i < n; i++) acc += numbers[i];
-  return acc/n;
+void lerUmidSolo(){
+  umidadeSolo = map(humSolo, 0, 1023, 0, 100);
+}
+
+void displaySerial(){
+  Serial.print(temperatura);
+  Serial.print(" C \t");
+  Serial.print(umidadeAr);
+  Serial.print(" % \t");
+  Serial.print(umidadeSolo);
+  Serial.println(" %");
+}
+
+void movingAverage(){
+  long accT = 0;
+  long accUA = 0;
+  long accUS = 0;
+  for(int i = n-1; i > 0; i--){
+    temperaturas[i] = temperaturas[i - 1];
+    umidadesAr[i] = umidadesAr[i - 1];
+    umidadesSolo[i] = umidadesSolo[i - 1];
+  }
+  
+  temperaturas[0] = temperatura;
+  umidadesAr[0] = umidadeAr;
+  umidadesSolo[0] = umidadeSolo;
+  
+  for(int i = 0; i < n; i++){
+    accT += temperaturas[i];
+    accUA += umidadesAr[i];
+    accUS += umidadesSolo[i];
+  }
+  
+  temperatura = accT/n;
+  umidadeAr = accUA/n;
+  umidadeSolo = accUS/n;
 }
